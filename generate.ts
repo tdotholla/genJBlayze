a9bb34668835a7084094c362eb6bdf3b1a48c84f
Kiel Byrne <kbyrne@costar.com>
2021-10-06 22:23:36 -0400

    reorg

--- generate.ts
+++ generate.ts
@@ -1,6 +1,6 @@
+import { buildSetup, createFiles, createMetaData } from "./src/main";
+import { defaultEdition } from "./src/config";
 const myArgs = process.argv.slice(2);
-const { buildSetup, createFiles, createMetaData } = require("./src/main.js");
-const { defaultEdition } = require("./src/config.js");
 const edition = myArgs.length > 0 ? Number(myArgs[0]) : defaultEdition;
 
 (() => {
