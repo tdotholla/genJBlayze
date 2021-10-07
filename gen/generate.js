d37e356240a4faffe1710c9802b06ff2aa3349f3
Kiel Byrne <kbyrne@costar.com>
2021-10-06 23:32:42 -0400

    ok...fixed

--- gen/generate.js
+++ gen/generate.js
@@ -1,6 +1,6 @@
 const myArgs = process.argv.slice(2);
-const { buildSetup, createFiles, createMetaData } = require("./src/main.js");
-const { defaultEdition } = require("./src/config.js");
+const { buildSetup, createFiles, createMetaData } = require("./main.js");
+const { defaultEdition } = require("./config.js");
 const edition = myArgs.length > 0 ? Number(myArgs[0]) : defaultEdition;
 
 (() => {
