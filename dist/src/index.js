976047491f7a02b15da6f07df300bea6caa21e39
Kiel Byrne <kiel.byrne@gmail.com>
2022-05-30 18:07:54 -0400

    update gitignore

--- dist/src/index.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-ReactDOM.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }), document.getElementById('root'));
-//# sourceMappingURL=index.js.map
